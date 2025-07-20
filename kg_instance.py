2fbf52a856e59720cbaa930558a0fcca660db3c1
Con1105 <constantinospaphitis@gmail.com>
2025-07-20 23:48:14 +0100

    Update kg_instance.py

--- kg_instance.py
+++ kg_instance.py
@@ -7,6 +7,10 @@ def set_kg_instance(instance):
     global kg
     kg = instance
 
+def set_thread_instance(instance):
+    global kg_thread_id
+    kg_thread_id = instance
+
 def get_kg_instance():
     if kg is None:
         raise RuntimeError("KGGen instance is not initialized. Make sure main.py ran first.")
