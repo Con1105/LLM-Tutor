3feba198d753fc339c5f6cb74bffd4d22cdd3064
Con1105 <constantinospaphitis@gmail.com>
2025-07-20 23:07:40 +0100

    Update main1.py

--- main1.py
+++ main1.py
@@ -17,7 +17,7 @@ if __name__ == "__main__":
     kg = KGGen(
         model="openai/gpt-4o",
         temperature=0.0,
-        api_key="your-api-key"
+        api_key="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"
     )
 
     # Replace sys.argv so Streamlit thinks it was launched from CLI
