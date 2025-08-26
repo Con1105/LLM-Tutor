eb6f2a39f72baba8bd9d94472803ff2e0dd61154
Con1105 <constantinospaphitis@gmail.com>
2025-08-26 02:37:01 +0100

    Update lesson.py

--- lesson.py
+++ lesson.py
@@ -1,18 +1,19 @@
-import streamlit as st
+# import streamlit as st
 
-# Set page title and style to match app.py
-st.set_page_config(page_title="Text Input Page", layout="wide")
-st.title("Knowledge Graph Curriculum Customizer")
-st.write("Enter your text below. This page uses the same style as the main app, but is fully independent.")
+# # Set page title and style to match app.py
+# st.set_page_config(page_title="Text Input Page", layout="wide")
+# st.title("Knowledge Graph Curriculum Customizer")
+# st.write("Enter your text below. This page uses the same style as the main app, but is fully independent.")
 
-# Text input area
-user_text = st.text_area("Paste your text here:", height=200)
+# # Text input area
+# user_text = st.text_area("Paste your text here:", height=200)
 
-# Show the input value (for demonstration)
-if user_text:
-    st.success("Text stored in variable!")
-    st.write(user_text)
-else:
-    st.info("Please enter some text above.")
+# # Show the input value (for demonstration)
+# if user_text:
+#     st.success("Text stored in variable!")
+#     st.write(user_text)
+# else:
+#     st.info("Please enter some text above.")
 
-# The variable 'user_text' contains the input text for further processing 
+
+# # The variable 'user_text' contains the input text for further processing 
